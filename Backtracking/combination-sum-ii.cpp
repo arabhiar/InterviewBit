7fdb1b507ded218db56f3d00cdef9f1ceabff985
Abhishek Ranjan <abhishekrotary2002@gmail.com>
2021-04-12 12:56:04 +0530

    combination-sum-ii.cpp

---
+++ Backtracking/combination-sum-ii.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void backtrack(vector<vector<int>> &res, vector<int> &A, vector<int> &track, int B, int currSum, int idx)
+{
+    if (currSum == B && (find(res.begin(), res.end(), track) == res.end()))
+    {
+        res.push_back(track);
+        return;
+    }
+    for (int i = idx; i < A.size(); i++)
+    {
+        if (currSum + A[i] > B)
+        {
+            continue;
+        }
+        track.push_back(A[i]);
+        backtrack(res, A, track, B, currSum + A[i], i + 1);
+        track.pop_back();
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> A(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+    int B;
+    cin >> B;
+
+    // set<int> s;
+    // for (int i = 0; i < A.size(); i++)
+    // {
+    //     s.insert(A[i]);
+    // }
+    // vector<int> temp;
+    // for (auto it : s)
+    // {
+    //     temp.push_back(it);
+    // }
+    sort(A.begin(), A.end());
+    vector<vector<int>> res;
+    vector<int> track;
+    backtrack(res, A, track, B, 0, 0);
+
+    for (int i = 0; i < res.size(); i++)
+    {
+        for (int j = 0; j < res[i].size(); j++)
+        {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
