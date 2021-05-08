4c0600a01a50bb93953115807e498d92d2631038
Abhishek Ranjan <abhishekrotary2002@gmail.com>
2021-05-08 08:37:50 +0530

    invert-the-binary-tree.cpp

---
+++ Tree Data Structure/invert-the-binary-tree.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define COUNT 3
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// Visualization of Tree
+void print2DUtil(TreeNode *root, int space)
+{
+    if (root == NULL)
+        return;
+    space += COUNT;
+    print2DUtil(root->right, space);
+    cout << endl;
+    for (int i = COUNT; i < space; i++)
+        cout << " ";
+    cout << root->val << "\n";
+    print2DUtil(root->left, space);
+}
+
+void print2D(TreeNode *root)
+{
+    print2DUtil(root, 0);
+}
+
+TreeNode *helper(TreeNode *A)
+{
+    if (A == NULL)
+    {
+        return NULL;
+    }
+    swap(A->left, A->right);
+    helper(A->left);
+    helper(A->right);
+    return A;
+}
+
+int main()
+{
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right = new TreeNode(3);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
+
+    print2D(root);
+    auto newRoot = helper(root);
+    print2D(newRoot);
+
+    cout << endl;
+    return 0;
+}
