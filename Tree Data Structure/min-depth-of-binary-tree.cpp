9d764a4d080d610549830401dd8f2d0b7b4e7b43
Abhishek Ranjan <abhishekrotary2002@gmail.com>
2021-05-09 13:33:47 +0530

    min-depth-of-binary-tree.cpp

---
+++ Tree Data Structure/min-depth-of-binary-tree.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define COUNT 3
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+// Visualization of Tree
+void print2DUtil(TreeNode *root, int space)
+{
+    if (root == NULL)
+        return;
+    space += COUNT;
+    print2DUtil(root->right, space);
+    cout << endl;
+    for (int i = COUNT; i < space; i++)
+        cout << " ";
+    cout << root->val << "\n";
+    print2DUtil(root->left, space);
+}
+
+void print2D(TreeNode *root)
+{
+    print2DUtil(root, 0);
+}
+
+int helper(TreeNode *A)
+{
+    if (A == NULL)
+    {
+        return 0;
+    }
+    if (A->left && !A->right)
+    {
+        return 1 + helper(A->left);
+    }
+    if (A->right && !A->left)
+    {
+        return 1 + helper(A->right);
+    }
+    return 1 + min(helper(A->left), helper(A->right));
+}
+
+int main()
+{
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->left->left = new TreeNode(4);
+    // root->left->right = new TreeNode(5);
+    // root->right = new TreeNode(3);
+    // root->right->left = new TreeNode(6);
+    // root->right->right = new TreeNode(7);
+
+    int ans = helper(root);
+
+    cout << ans;
+    cout << endl;
+
+    return 0;
+}
