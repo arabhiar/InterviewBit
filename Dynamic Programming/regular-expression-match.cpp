62cf7e77e85468c9400daceeea600ce16743bf3f
Abhishek Ranjan <abhishekrotary2002@gmail.com>
2021-05-15 23:55:41 +0530

    regular-expression-match.cpp

---
+++ Dynamic Programming/regular-expression-match.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int helper(string A, string B)
+{
+    // ’?’ : Matches any single character.
+    // ‘*’ : Matches any sequence of characters (including the empty sequence).
+    int l1 = A.length(), l2 = B.length();
+    int count = 0;
+    for (auto ch : B)
+    {
+        if (ch != '*')
+        {
+            count++;
+        }
+    }
+    if (count > l1)
+    {
+        return 0;
+    }
+    vector<vector<int>> dp(l1 + 1, vector<int>(l2 + 1, 0));
+    dp[0][0] = 1;
+    int idx = 1;
+    while (B[idx - 1] == '*')
+    {
+        dp[0][idx] = 1;
+        idx++;
+    }
+    for (int i = 1; i <= l1; i++)
+    {
+        for (int j = 1; j <= l2; j++)
+        {
+            if (B[j - 1] == '*')
+            {
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - 1] || dp[i][j - 1];
+            }
+            else if (B[j - 1] != '?' && B[j - 1] != A[i - 1])
+            {
+                dp[i][j] = 0;
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+        }
+    }
+    return dp[l1][l2];
+}
+
+int main()
+{
+    string A, B;
+    cin >> A >> B;
+
+    cout << helper(A, B);
+
+    cout << endl;
+    return 0;
+}
