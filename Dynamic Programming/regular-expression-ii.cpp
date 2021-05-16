66b9e5024949f2d4084e14c1d6f514d6a375dbfd
Abhishek Ranjan <abhishekrotary2002@gmail.com>
2021-05-16 15:40:20 +0530

    regular-expression.cpp

--- Dynamic Programming/regular-expression-ii.cpp
+++ Dynamic Programming/regular-expression-ii.cpp
@@ -3,42 +3,23 @@ using namespace std;
 
 int helper(string A, string B)
 {
-    // ’?’ : Matches any single character.
-    // ‘*’ : Matches any sequence of characters (including the empty sequence).
+    // '.' Matches any single character.
+    // '*' Matches zero or more of the preceding element.
     int l1 = A.length(), l2 = B.length();
-    int count = 0;
-    for (auto ch : B)
-    {
-        if (ch != '*')
-        {
-            count++;
-        }
-    }
-    if (count > l1)
-    {
-        return 0;
-    }
     vector<vector<bool>> dp(l1 + 1, vector<bool>(l2 + 1, false));
     dp[0][0] = 1;
-    for (int i = 1; i <= l1; i++)
+    for (int i = 0; i <= l1; i++)
     {
         for (int j = 1; j <= l2; j++)
         {
-            if (B[j - 1] == '*' && B[j - 2] == A[i - 1])
+            if (B[j - 1] != '*')
             {
-                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - 1] || dp[i][j - 1];
+                dp[i][j] = (i > 0) && (A[i - 1] == B[j - 1] || B[j - 1] == '.') && dp[i - 1][j - 1];
             }
-            else if (B[j - 1] == '*')
-            {
-                dp[i][j] = dp[i][j - 1];
-            }
-            else if (B[j - 1] != '.' && B[j - 1] != A[i - 1])
-            {
-                dp[i][j] = 0;
-            }
+            
             else
             {
-                dp[i][j] = dp[i - 1][j - 1];
+                dp[i][j] = dp[i][j - 2] || (i > 0 && (B[j - 2] == A[i - 1] || B[j - 2] == '.') && dp[i - 1][j]);
             }
         }
     }
