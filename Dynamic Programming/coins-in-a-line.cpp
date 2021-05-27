5a9082e1794eb5382b8327095e9445901756a061
Abhishek Ranjan <abhishekrotary2002@gmail.com>
2021-05-27 12:27:22 +0530

    coins-in-a-line.cpp

--- Dynamic Programming/coins-in-a-line.cpp
+++ Dynamic Programming/coins-in-a-line.cpp
@@ -1,32 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// https://algorithms.tutorialhorizon.com/dynamic-programming-coin-in-a-line-game-problem/
 int helper(vector<int> A)
 {
-    int i = 0, j = A.size() - 1;
-    int ans = 0;
-    while (i < j)
+    vector<vector<int>> dp(A.size(), vector<int>(A.size(), 0));
+    for (int block_size = 1; block_size <= A.size(); block_size++)
     {
-        if (A[i] > A[j])
+        for (int start = 0; start <= A.size() - block_size; start++)
         {
-            ans += A[i];
-            i++;
-        }
-        else
-        {
-            ans += A[j];
-            j--;
-        }
-        if (A[i] > A[j])
-        {
-            i++;
-        }
-        else
-        {
-            j--;
+            int end = start + block_size - 1;
+            if (start == end)
+            {
+                dp[start][end] = A[start];
+                continue;
+            }
+            if (start + 1 == end)
+            {
+                dp[start][end] = max(A[start], A[end]);
+            }
+            else
+            {
+                int option1 = A[start] + min(dp[start + 2][end], dp[start + 1][end - 1]); // possible value if player 2 is playing optimally and i select start index
+                int option2 = A[end] + min(dp[start + 1][end - 1], dp[start][end - 2]);  // possible value if player 2 is playing optimally and i select end index
+                dp[start][end] = max(option1, option2); // choosing max among bot option
+            }
         }
     }
-    return ans;
+    return dp[0][A.size() - 1];
 }
 
 int main()
@@ -36,7 +37,7 @@ int main()
     vector<int> A(n);
     for (int i = 0; i < n; i++)
         cin >> A[i];
-    
+
     cout << helper(A);
 
     cout << endl;
